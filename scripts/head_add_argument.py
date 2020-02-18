3dde54900e946d1b19875e898001314afcf98588
Sung-Huan (Silas) Yu <sung-huan.yu@uni-wuerzburg.de>
2020-02-18 14:30:09 +0100

    set default value to nrow

--- scripts/head_add_argument.py
+++ scripts/head_add_argument.py
@@ -4,7 +4,7 @@ from perseuspy import pd
 parser = argparse.ArgumentParser("Head processing")
 parser.add_argument("input", help="path of the input file")
 parser.add_argument("output", help="path of the output file")
-parser.add_argument("--nrow", type=int, help="the number of row")
+parser.add_argument("--nrow", type=int, default=15, help="the number of row")
 
 arg = parser.parse_args()
 
